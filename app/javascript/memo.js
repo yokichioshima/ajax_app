90873ba0078968e830f49aa57007e0236f1b1fd2
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2021-12-30 14:26:04 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,8 +1,6 @@
-const e = require("turbolinks");
-
 function post (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", () => {
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);

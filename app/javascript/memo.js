f5c50508e2327a7b3271360a4fd2571d6c58fb7b
yokichioshima <96287511+yokichioshima@users.noreply.github.com>
2021-12-30 12:28:42 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,16 @@
+const e = require("turbolinks");
+
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", () => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
